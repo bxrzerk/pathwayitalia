a7e6573a87e38bc347ffe7155d0bb1293e59abf9
sohaib <87718449+sohaibyyy@users.noreply.github.com>
2026-06-09 15:10:50 +0200

    Serve RealFaviconGenerator asset paths

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,15 @@ type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
 };
 
+const faviconAssetRedirects = new Map([
+  ["/favicon.svg", "https://realfavicongenerator.net/files/43c55f8b-ab7f-488e-bc9e-4c4122a72b9d/favicon.svg"],
+  ["/favicon-96x96.png", "https://realfavicongenerator.net/files/43c55f8b-ab7f-488e-bc9e-4c4122a72b9d/favicon-96x96.png"],
+  ["/favicon.ico", "https://realfavicongenerator.net/files/43c55f8b-ab7f-488e-bc9e-4c4122a72b9d/favicon.ico"],
+  ["/apple-touch-icon.png", "https://realfavicongenerator.net/files/43c55f8b-ab7f-488e-bc9e-4c4122a72b9d/apple-touch-icon.png"],
+  ["/web-app-manifest-192x192.png", "https://realfavicongenerator.net/files/43c55f8b-ab7f-488e-bc9e-4c4122a72b9d/web-app-manifest-192x192.png"],
+  ["/web-app-manifest-512x512.png", "https://realfavicongenerator.net/files/43c55f8b-ab7f-488e-bc9e-4c4122a72b9d/web-app-manifest-512x512.png"],
+]);
+
 let serverEntryPromise: Promise<ServerEntry> | undefined;
 
 async function getServerEntry(): Promise<ServerEntry> {
@@ -16,6 +25,14 @@ async function getServerEntry(): Promise<ServerEntry> {
     );
   }
   return serverEntryPromise;
+}
+
+function redirectFaviconAsset(request: Request): Response | undefined {
+  const pathname = new URL(request.url).pathname;
+  const target = faviconAssetRedirects.get(pathname);
+  if (!target) return undefined;
+
+  return Response.redirect(target, 302);
 }
 
 // h3 swallows in-handler throws into a normal 500 Response with body
@@ -39,6 +56,9 @@ async function normalizeCatastrophicSsrResponse(response: Response): Promise<Res
 
 export default {
   async fetch(request: Request, env: unknown, ctx: unknown) {
+    const faviconRedirect = redirectFaviconAsset(request);
+    if (faviconRedirect) return faviconRedirect;
+
     try {
       const handler = await getServerEntry();
       const response = await handler.fetch(request, env, ctx);
